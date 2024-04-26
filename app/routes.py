735ac23c9209342a376b119b02d6da0894e467dc
researchersec <109282836+researchersec@users.noreply.github.com>
2024-04-26 03:51:54 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,10 @@
+from flask import Blueprint, jsonify
+import json
 
+main = Blueprint('main', __name__)
+
+@main.route('/data')
+def data():
+    with open('latest.json', 'r') as f:
+        data = json.load(f)
+    return jsonify(data)

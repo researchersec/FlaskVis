b6e0599b75664ad969986cef62ce2354b6b33241
researchersec <109282836+researchersec@users.noreply.github.com>
2024-04-26 03:54:31 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 def create_app():
     app = Flask(__name__)
+    socketio = SocketIO(app)
     from .routes import main
     app.register_blueprint(main)
-    return app
+    return app, socketio
